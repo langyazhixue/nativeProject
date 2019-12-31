2ff8e43884d9573da04ad163231f65232f7ee390
jiaxiaoxiao <jiaxiaoxiao@zhexinit.com>
2019-12-31 18:01:59 +0800

    update

--- App.js
+++ App.js
@@ -6,14 +6,21 @@
  * @flow
  */
 
-import React from 'react';
+import React, {Component} from 'react';
 import {
   SafeAreaView,
   StyleSheet,
   ScrollView,
   View,
   Text,
+  Button,
+  TouchableWithoutFeedback,
+  TouchableHighlight,
   StatusBar,
+  Alert,
+  Image,
+  TextInput,
+  ActivityIndicator,
 } from 'react-native';
 
 import {
@@ -23,92 +30,135 @@ import {
   DebugInstructions,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+// var Dimensions = require('Dimensions');
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      animating: false,
+    };
+  }
+  render() {
+    return (
+      <>
+        <StatusBar barStyle="dark-content" />
+        {/* 刘海屏幕 */}
+        <SafeAreaView style={styles.containerStyle}>
+          <ScrollView
+            contentInsetAdjustmentBehavior="automatic"
+            style={styles.containerStyle}>
+            <View style={styles.buttonView}>
+              <View>
+                <Text>11</Text>
+              </View>
+              <TouchableWithoutFeedback
+                onPress={() => {
+                  console.log('onPress');
+                }}
+                onPressIn={() => {
+                  console.log('onPressIn');
+                }}
+                onPressOut={() => {
+                  console.log('onPressOut');
+                }}
+                onLongPress={() => {
+                  console.log('onLongPress');
+                }}>
+                <View style={styles.button}>
+                  <Text>点击</Text>
+                </View>
+              </TouchableWithoutFeedback>
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
+              <TouchableHighlight
+                onPress={() => {
+                  console.log('onPress');
+                }}
+                activeOpacity={0.1}
+                underlayColor="blue">
+                <View style={styles.button}>
+                  <Text>点击</Text>
+                </View>
+              </TouchableHighlight>
+              <Button
+                title={'点击'}
+                color="red"
+                onPress={() => {
+                  this.setState({
+                    animating: !this.state.animating,
+                  });
+                  // Alert.alert('dianjil');
+                }}
+              />
+
+              <ActivityIndicator
+                size="large"
+                color="red"
+                animating={this.state.animating}
+              />
+
+              <Image source={require('./images/log.jpg')} />
+              {/* 显示网络图片 */}
+              <Image
+                style={{width: 50, height: 50}}
+                source={{
+                  uri:
+                    'https://facebook.github.io/react-native/docs/assets/favicon.png',
+                }}
+              />
+              <View style={{width: '100%'}}>
+                <TextInput
+                  style={styles.textInput}
+                  placeholder="请输入6为数字密码"
+                  value={'default'}
+                  placeholderTextColor={'red'}
+                  keyboardType={'number-pad'}
+                  clearButtonMode={'while-editing'}
+                  onChangeText={text => {
+                    console.log(text);
+                  }}
+                  returnKeyType="done"
+                  onSubmitEditing={event => {
+                    console.log(event);;
+                  }}
+                />
+              </View>
             </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+          </ScrollView>
+        </SafeAreaView>
+      </>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  containerStyle: {
+    backgroundColor: '#fff',
+    minHeight: 500,
+    flex: 1,
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
+  buttonView: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    display: 'flex',
+    flexDirection: 'column',
   },
-  body: {
-    backgroundColor: Colors.white,
+  button: {
+    width: 50,
+    height: 50,
+    display: 'flex',
+    backgroundColor: 'red',
+    borderWidth: 1,
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+  textInput: {
+    marginTop: 20,
+    marginHorizontal: 40,
+    height: 30,
+    borderWidth: 1,
+    borderColor: '#7fff00',
+    // width: '100%',
+    // boxSizing: 'border-box',
   },
 });
-
-export default App;
