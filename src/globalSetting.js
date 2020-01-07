f48f1a3cb5f846656bbc65ac942c07e0bbbf6198
jiaxiaoxiao <jiaxiaoxiao@zhexinit.com>
2020-01-07 11:57:25 +0800

    update

--- src/globalSetting.js
+++ src/globalSetting.js
@@ -8,6 +8,6 @@ export const defaultNavigationOptions = {
     fontWeight: '500',
     fontSize: variableStyle.barBarTitleFont,
   },
-  gestureResponseDistance: 100, // 定义滑动返回的有效距离，⽔水平状态下默认:25，垂直状态默认 135;
+  gestureResponseDistance: 100, // 定义滑动返回的有效距离，水平状态下默认:25，垂直状态默认 135;
   //gesturesEnabled: false,
 };
